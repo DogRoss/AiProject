fda987ee8ae9eaf4f9b906c912925c918fff6c66
DogRoss <edgaragunther@gmail.com>
2022-04-11 17:55:53 -0700

    added necessitated classes // need pathfinding to be added and AI to utilize said class

---
+++ AIEYear1Samples-master/RaylibStarterCPP/GridMap.cpp
@@ -0,0 +1,1 @@
+#include "GridMap.h"
---
+++ AIEYear1Samples-master/RaylibStarterCPP/GridMap.h
@@ -0,0 +1,11 @@
+#pragma once
+class GridMap
+{
+	/*
+	width/height of node map (number of nodes on each axis)
+	node radius (radius is half the full length of a side)
+
+	*/
+
+};
+
